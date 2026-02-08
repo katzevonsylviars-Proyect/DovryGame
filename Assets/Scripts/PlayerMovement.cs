0f6ceb42dc190d50abbcf85092394e8c8decd4dd
katzevonsylviars-Proyect <katzevonsylviar@gmail.com>
2026-02-08 17:39:50 -0300

    Actualizado 08/02
    No se que cambios se añade (pueden ser):
    - Sprite agregado
    - Dash agregado
    - Doble salto agregado
    - Revisado colision de materiales

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,37 +8,81 @@ public class PlayerMovement : MonoBehaviour
     public float LongitudRaycast = 0.1f;
     public LayerMask CapaDeSuelo;
 
+    public float combustible = 100;
+
+    public int saltosMaximos = 1;
+
+    public float velocidadDash = 15f;
+    public float duracionDash = 0.2f;
+    public float cooldownDash = 1f;
+
     public bool EnElSuelo;
+
+    private int saltosRestantes;
+    private bool estaDasheando;
+    private float tiempoDash;
+    private float proximoDash;
+
     private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        saltosRestantes = saltosMaximos;
     }
 
     void Update()
     {
+        // Comprobación de suelo
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, LongitudRaycast, CapaDeSuelo);
+        EnElSuelo = hit.collider != null;
+
+        if (EnElSuelo)
+        {
+            saltosRestantes = saltosMaximos;
+        }
+
+        // DASH
+        if (Input.GetKeyDown(KeyCode.LeftShift) && Time.time >= proximoDash)
+        {
+            estaDasheando = true;
+            tiempoDash = duracionDash;
+            proximoDash = Time.time + cooldownDash;
+        }
+
+        if (estaDasheando)
+        {
+            float direccion = transform.localScale.x;
+            rb.linearVelocity = new Vector2(direccion * velocidadDash, 0);
+            tiempoDash -= Time.deltaTime;
+
+            if (tiempoDash <= 0)
+            {
+                estaDasheando = false;
+            }
+            return;
+        }
+
+        // Movimiento horizontal
         float VelocidadX = Input.GetAxis("Horizontal");
 
         if (VelocidadX < 0)
         {
-            transform.localScale = new Vector3(1,1,1);
+            transform.localScale = new Vector3(-1, 1, 1);
         }
-        Vector3 posicion = transform.position;
-
-        //transform.position = new Vector3(VelocidadX + posicion.x, posicion.y, posicion.z);
-    
-        Vector2 NuevaVelocidad = new Vector2(VelocidadX * velocidad, rb.linearVelocity.y);
-        rb.linearVelocity = NuevaVelocidad;
-
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, LongitudRaycast, CapaDeSuelo);
-        EnElSuelo = hit.collider != null;
-            
-
-
-        if(EnElSuelo && Input.GetKeyDown(KeyCode.Space))
+        else if (VelocidadX > 0)
         {
-            rb.AddForce(new Vector2(0f,FuerzaDeSalto), ForceMode2D.Impulse);
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        rb.linearVelocity = new Vector2(VelocidadX * velocidad, rb.linearVelocity.y);
+
+        // SALTO / DOBLE SALTO
+        if (Input.GetKeyDown(KeyCode.Space) && saltosRestantes > 0)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+            rb.AddForce(Vector2.up * FuerzaDeSalto, ForceMode2D.Impulse);
+            saltosRestantes--;
         }
     }
 
